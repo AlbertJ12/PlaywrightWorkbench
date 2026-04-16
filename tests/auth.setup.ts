3a9bbccedc67e184ea686817ee3c0efbf38ecdc4
Albert Colon <albertjoel.cg@gmail.com>
2026-04-16 15:44:07 -0400

    Added wait for loadstate in setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,6 +6,7 @@ setup("Create customer 01 auth", async ({page}) => {
     const customer01AuthFile = ".auth/customer01.json";
 
     await page.goto('https://practicesoftwaretesting.com/auth/login');
+    await page.waitForLoadState('networkidle');
     await page.getByTestId('email').fill(email);
     await page.getByTestId('password').fill(password);
     await page.getByTestId('login-submit').click();
